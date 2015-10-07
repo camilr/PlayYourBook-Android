12914f0b25e70fd70511d444875751eded9658e4
Camila <camilalr93@gmail.com>
2015-10-07 02:04:30 -0500

    Explorador de Archivos. Abrir un archivo (Prueba)

--- app/src/main/java/movil/camilr/playyourbooks/OpenFileActivity.java
+++ app/src/main/java/movil/camilr/playyourbooks/OpenFileActivity.java
@@ -1,11 +1,19 @@
 package movil.camilr.playyourbooks;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.graphics.Rect;
+import android.graphics.pdf.PdfRenderer;
+import android.os.ParcelFileDescriptor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import movil.camilr.playyourbooks.entities.FilePdf;
@@ -13,6 +21,8 @@ import movil.camilr.playyourbooks.fragments.LocalFragment;
 
 public class OpenFileActivity extends AppCompatActivity {
 
+    int currentPage = 0;
+    ImageView openFile;
     int posicionArchivo;
     List<FilePdf> data;
     FilePdf file;
@@ -28,6 +38,10 @@ public class OpenFileActivity extends AppCompatActivity {
         data = file.listAll(FilePdf.class);
 
         file = data.get(posicionArchivo);
+
+
+
+        renderPdf();
 
     }
 
@@ -51,5 +65,37 @@ public class OpenFileActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void renderPdf(){
+        try {
+
+            openFile = (ImageView) findViewById(R.id.open_file);
+            int REQ_WIDTH = 1;
+            int REQ_HEIGHT = 1;
+            REQ_WIDTH = openFile.getWidth();
+            REQ_HEIGHT = openFile.getHeight();
+
+            Bitmap bitmap = Bitmap.createBitmap(REQ_WIDTH, REQ_HEIGHT, Bitmap.Config.ARGB_4444);
+            File file2 = new File("/storage/emulated/0/bluetooth/PMBOOK.PMI.pdf.pdf");
+
+            PdfRenderer render = new PdfRenderer(ParcelFileDescriptor.open(file2, ParcelFileDescriptor.MODE_READ_ONLY));
+
+            if (currentPage < 0) {
+                currentPage = 0;
+            } else if (currentPage > render.getPageCount()) {
+                currentPage = render.getPageCount() - 1;
+            }
+
+            Matrix m = openFile.getImageMatrix();
+            Rect rect = new Rect(0,0,REQ_WIDTH,REQ_HEIGHT);
+            render.openPage(6).render(bitmap,rect,m,PdfRenderer.Page.RENDER_MODE_FOR_DISPLAY);
+            openFile.setImageMatrix(m);
+            openFile.setImageBitmap(bitmap);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
